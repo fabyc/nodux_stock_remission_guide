8f798d28d3f3a6d84e8ba6def93c3ab17b31d209
Tatiana Quizhpe <tatianaq@nodux.ec>
2017-01-04 12:45:54 -0500

    Default quantity

---
+++ move.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+
+import collections
+import logging
+from decimal import Decimal
+from trytond.pyson import Eval
+from trytond.pyson import Id
+from trytond.model import ModelSQL, Workflow, fields, ModelView
+from trytond.transaction import Transaction
+from trytond.wizard import Wizard, StateTransition, StateView, Button
+from trytond.pool import Pool, PoolMeta
+
+__all__ = ['Move']
+__metaclass__ = PoolMeta
+
+class Move():
+    "Stock Move"
+    __name__ = 'stock.move'
+
+    @classmethod
+    def __setup__(cls):
+        super(Move, cls).__setup__()
+
+    @classmethod
+    def default_quantity(cls):
+        return 1
